2938efe06220684dcfb37d093f542fa05edbb4a5
Yang.Li <yang.li@polyvirtual.com>
2017-11-27 15:36:10 +0800

    Add MainController.java

---
+++ multimodules-web/src/main/java/com/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+	
+    @RequestMapping("/")
+    public ResponseEntity<?> getSexAll() {
+
+        ResponseEntity<String> responseEntity = new ResponseEntity<>("Hello World!", HttpStatus.OK);
+
+        return responseEntity;
+    }
+}
