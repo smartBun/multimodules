0b649606b68f0270eec487f835af32f784606b75
Yang <yang.li@polyvirtual.com>
2017-12-28 15:59:52 +0800

    State Pattern

---
+++ multimodules-web/src/test/java/com/learn/designPattern/behavioralPatterns/state/Client.java
@@ -0,0 +1,12 @@
+package com.learn.designPattern.behavioralPatterns.state;
+
+public class Client {
+    public static void main(String[] args){
+        Context context=new Context();
+        context.setMp3State(new PowerOffState());
+        context.preSong();
+        context.powerOn();
+        context.nextSong();
+        context.powerOff();
+    }
+}
