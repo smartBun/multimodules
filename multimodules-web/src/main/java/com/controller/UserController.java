81d78f73873db57e7cc05db2333fd4df1e7c21ff
liyang <liyang>
2018-06-07 21:41:42 +0800

    JWT

--- multimodules-web/src/main/java/com/controller/UserController.java
+++ multimodules-web/src/main/java/com/controller/UserController.java
@@ -2,11 +2,13 @@ package com.controller;
 
 import com.entity.User;
 import com.service.IUserService;
+import com.utils.JSONResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -14,16 +16,27 @@ import java.util.List;
  * 用户管理Controller
  */
 @RestController
-@RequestMapping("/user")
 public class UserController {
 
     @Autowired
     private IUserService userService;
 
     @GetMapping
-    @RequestMapping("/all")
-    public List<User> findUsers() {
+    @RequestMapping(value = "/users", produces="application/json;charset=UTF-8")
+    public List<User> userList() {
         return userService.findAll();
+    }
+
+    @RequestMapping(value = "/hello", produces="application/json;charset=UTF-8")
+    public String hello() {
+        ArrayList<String> users =  new ArrayList<String>(){{ add("hello"); }};
+        return JSONResult.fillResultString(0, "", users);
+    }
+
+    @RequestMapping(value = "/world", produces="application/json;charset=UTF-8")
+    public String world() {
+        ArrayList<String> users =  new ArrayList<String>(){{ add("world"); }};
+        return JSONResult.fillResultString(0, "", users);
     }
 
 }
