1487e24a839b3ffb3bfa67e68910ed7e086efc19
Yang <yang.li@polyvirtual.com>
2018-06-07 19:22:18 +0800

    Integrate mybatis into project

--- multimodules-web/src/main/java/com/Application.java
+++ multimodules-web/src/main/java/com/Application.java
@@ -10,8 +10,6 @@ import org.springframework.context.annotation.Configuration;
 @SpringBootApplication
 public class Application {
 
-
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
