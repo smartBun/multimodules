f095473adf5a89949c41ec6da14b944196b0f435
Yang <yang.li@polyvirtual.com>
2018-06-07 19:35:37 +0800

    Add mapper scan

--- multimodules-web/src/main/java/com/Application.java
+++ multimodules-web/src/main/java/com/Application.java
@@ -1,12 +1,13 @@
 package com;
 
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//@EnableScheduling
+@MapperScan("com.dao")
 @SpringBootApplication
 public class Application {
 
